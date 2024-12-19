9b929fded51c1bf7d32008994b13b07d883dc2d0
Oluwatosin Adejayan <holuwarthosyn21@gmail.com>
2024-12-19 12:48:20 +0200

    pass

--- teleclinix-react/src/App.jsx
+++ teleclinix-react/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   <Router>
   <Routes>
   {/* Authentication Routes */}
-  {/* <Route path='/authentication/login' element={<LoginPage setLogin={setLogin} />} /> */}
+  <Route path='/authentication/login' element={<LoginPage setLogin={setLogin} />} />
   <Route path='/authentication/register' element={<RegisterPage />} />
   <Route path='/' element={<PatientDashboard />}/>
   <Route path='/schedule-patient' element={<SchedulePatient />}/>
